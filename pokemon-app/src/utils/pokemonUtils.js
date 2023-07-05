89369ade30ec57cb782954800be6a11bb0bec11a
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-07-05 15:44:11 +0200

    add utils function file

--- pokemon-app/src/utils/pokemonUtils.js
+++ pokemon-app/src/utils/pokemonUtils.js
@@ -1,3 +1,4 @@
+// Returns a string representation of the input number with a "#" symbol and leading zeros if necessary.
 export function getPokemonNumber(number) {
   return "#" + String(number).padStart(3, "0");
 }
