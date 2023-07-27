8e43d2c66c1b12db2676d87bdd5870cf76355bcc
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-07-27 08:53:14 +0200

    Refactor sidebar code and make it more DRY

--- pokemon-app/src/SharedComponents/Sidebar.js
+++ pokemon-app/src/SharedComponents/Sidebar.js
@@ -7,8 +7,16 @@ import { ReactComponent as FacebookSVG } from "../Assets//SVGs/Facebook.svg";
 import { ReactComponent as YoutubeSVG } from "../Assets//SVGs/Youtube.svg";
 import { ReactComponent as RedditSVG } from "../Assets//SVGs/Reddit.svg";
 
+const navLinks = [
+  { icon: TwitterSVG, label: "Pokemon Twitter" },
+  { icon: FacebookSVG, label: "Pokemon Facebook" },
+  { icon: YoutubeSVG, label: "Pokemon Youtube" },
+  { icon: RedditSVG, label: "Pokemon Reddit" },
+];
+
 export default function Sidebar() {
-  const linkLocation = useLocation().pathname === "/" ? "/favourites" : "/";
+  const { pathname } = useLocation();
+  const linkLocation = pathname === "/" ? "/favourites" : "/";
 
   return (
     <div className="sidebar">
@@ -23,50 +31,25 @@ export default function Sidebar() {
               />
             </li>
           </Link>
-          {useLocation().pathname === "/" ? (
-            <li className="sidebar-li">
-              <Link className="sidebar-a-href" to="/favourites">
-                <SideBarStarSVG />
-                <span className="sidebar-span">Favourite Pokemons</span>
-              </Link>
-            </li>
-          ) : (
-            <li className="sidebar-li">
-              <Link className="sidebar-a-href" to="/">
-                <HomeSVG />
-                <span className="sidebar-span">Home</span>
-              </Link>
-            </li>
-          )}
           <li className="sidebar-li">
-            <Link className="sidebar-a-href" to={linkLocation}>
-              <TwitterSVG />
-              <span className="sidebar-span">Pokemon Twitter</span>
-            </Link>
-          </li>
-          <li className="sidebar-li">
-            <Link className="sidebar-a-href" to={linkLocation}>
-              <FacebookSVG />
-              <span className="sidebar-span">Pokemon Facebook</span>
-            </Link>
-          </li>
-          <li className="sidebar-li">
-            <Link className="sidebar-a-href" to={linkLocation}>
-              <YoutubeSVG />
-              <span className="sidebar-span">Pokemon Youtube</span>
-            </Link>
-          </li>
-          <li className="sidebar-li">
-            <Link className="sidebar-a-href" to={linkLocation}>
-              <RedditSVG />
-              <span
-                href="https://www.reddit.com/r/pokemon/"
-                className="sidebar-span"
-              >
-                Pokemon Reddit
+            <Link
+              className="sidebar-a-href"
+              to={pathname === "/" ? "/favourites" : "/"}
+            >
+              {pathname === "/" ? <SideBarStarSVG /> : <HomeSVG />}
+              <span className="sidebar-span">
+                {pathname === "/" ? "Favourite Pokemons" : "Home"}
               </span>
             </Link>
           </li>
+          {navLinks.map(({ icon: Icon, label }) => (
+            <li key={label} className="sidebar-li">
+              <Link className="sidebar-a-href" to={linkLocation}>
+                <Icon />
+                <span className="sidebar-span">{label}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
