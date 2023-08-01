fc0e71ef55865fb7072f95466a687cc7c240dea0
Yousif Ali <Yousifali95@hotmail.co.uk>
2023-08-01 20:04:54 +0200

    Changed the fetch functions to using axios instead

--- pokemon-app/src/Hooks/useFetchPokemons.js
+++ pokemon-app/src/Hooks/useFetchPokemons.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { formatPokemonName } from "../utils/pokemonUtils";
+import axios from "axios";
 
 /**
  * A custom hook that fetches a list of Pokemons and returns an object mapping
@@ -12,24 +13,36 @@ import { formatPokemonName } from "../utils/pokemonUtils";
 
 export default function useFetchPokemons(MAX_POKEMON_NUMBER) {
   const [pokemonsObject, setPokemonsObject] = useState({});
+  const abortController = new AbortController();
 
   useEffect(() => {
-    fetch(
-      `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON_NUMBER}&offset=0`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        const newObject = Object.keys(json.results).reduce((result, index) => {
-          const { name } = json.results[index];
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON_NUMBER}&offset=0`,
+          { signal: abortController.signal }
+        );
+        const { data } = response;
+        const newObject = Object.keys(data.results).reduce((result, index) => {
+          const { name: pokemonName } = data.results[index];
 
-          result[formatPokemonName(name)] = parseInt(index) + 1;
+          result[formatPokemonName(pokemonName)] = parseInt(index) + 1;
           return result;
         }, {});
         setPokemonsObject(newObject);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      console.log("abortController.abort()");
+      abortController.abort();
+    };
   }, [MAX_POKEMON_NUMBER]);
 
   return pokemonsObject;
--- pokemon-app/src/Pages/SearchPage/Hooks/useFetchPokemonInformation.js
+++ pokemon-app/src/Pages/SearchPage/Hooks/useFetchPokemonInformation.js
@@ -1,81 +1,85 @@
-/**
- * A custom hook that fetches information about a Pokemon and returns the data.
- * It encapsulates a useEffect hook that listens for changes to the current Pokemon,
- * fetches the species and details of the Pokemon from the PokeAPI,
- * and returns the processed Pokemon data.
- *
- * @param {string} currentPokemon - The name of the current Pokemon.
- * @param {Object} pokemons - An object mapping formatted Pokemon names to their
- * pokedex numbers.
- * @returns {object} - An object containing information about the current Pokemon.
- */
-
 import { useEffect, useState } from "react";
 import {
   formatFlavorText,
   formatGeneration,
   formatCatchRate,
 } from "../../../utils/pokemonUtils";
+import axios from "axios";
 
 function useFetchPokemonInformation(currentPokemon, pokemons) {
   const [pokemonObject, setPokemonObject] = useState(null);
-
-  const fetchPokemonInformation = async () => {
-    try {
-      const newPokemonObject = {};
-
-      const speciesResponse = await fetch(
-        `https://pokeapi.co/api/v2/pokemon-species/${pokemons[currentPokemon]}`
-      );
-      const pokemonResponse = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${pokemons[currentPokemon]}`
-      );
-
-      const [speciesData, pokemonDetails] = await Promise.all([
-        speciesResponse.json(),
-        pokemonResponse.json(),
-      ]);
-
-      newPokemonObject["name"] = pokemonDetails.name;
-
-      newPokemonObject["generation"] = formatGeneration(
-        speciesData.generation.name
-      );
-
-      const types = pokemonDetails.types.map((typeData) => typeData.type.name);
-      newPokemonObject["types"] = types;
-
-      const category = speciesData.genera.find(
-        (genus) => genus.language.name === "en"
-      );
-      newPokemonObject["category"] = category.genus;
-
-      const flavorText = speciesData.flavor_text_entries.find(
-        (entry) => entry.language.name === "en"
-      );
-      newPokemonObject["flavorText"] = formatFlavorText(flavorText.flavor_text);
-
-      newPokemonObject["catchRate"] = formatCatchRate(speciesData.capture_rate);
-
-      newPokemonObject["color"] = speciesData.color.name;
-
-      newPokemonObject["height"] = (
-        parseFloat(pokemonDetails.height) / 10
-      ).toFixed(1);
-
-      newPokemonObject["weight"] = (
-        parseFloat(pokemonDetails.weight) / 10
-      ).toFixed(1);
-
-      setPokemonObject(newPokemonObject);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+  const abortController = new AbortController();
 
   useEffect(() => {
-    fetchPokemonInformation();
-  }, [currentPokemon]);
+    const fetchPokemonInformation = async () => {
+      try {
+        const newPokemonObject = {};
+
+        const speciesResponse = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon-species/${pokemons[currentPokemon]}`,
+          { signal: abortController.signal }
+        );
+        const pokemonResponse = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemons[currentPokemon]}`,
+          { signal: abortController.signal }
+        );
+
+        const [speciesData, pokemonDetails] = await Promise.all([
+          speciesResponse.data,
+          pokemonResponse.data,
+        ]);
+
+        newPokemonObject["name"] = pokemonDetails.name;
+
+        newPokemonObject["generation"] = formatGeneration(
+          speciesData.generation.name
+        );
+
+        const types = pokemonDetails.types.map(
+          (typeData) => typeData.type.name
+        );
+        newPokemonObject["types"] = types;
+
+        const category = speciesData.genera.find(
+          (genus) => genus.language.name === "en"
+        );
+        newPokemonObject["category"] = category.genus;
+
+        const flavorText = speciesData.flavor_text_entries.find(
+          (entry) => entry.language.name === "en"
+        );
+        newPokemonObject["flavorText"] = formatFlavorText(
+          flavorText.flavor_text
+        );
+
+        newPokemonObject["catchRate"] = formatCatchRate(
+          speciesData.capture_rate
+        );
+
+        newPokemonObject["color"] = speciesData.color.name;
+
+        newPokemonObject["height"] = (
+          parseFloat(pokemonDetails.height) / 10
+        ).toFixed(1);
+
+        newPokemonObject["weight"] = (
+          parseFloat(pokemonDetails.weight) / 10
+        ).toFixed(1);
+
+        setPokemonObject(newPokemonObject);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    if (pokemons[currentPokemon]) fetchPokemonInformation();
+
+    return () => {
+      abortController.abort();
+    };
+  }, [currentPokemon, pokemons]);
 
   return pokemonObject;
 }
